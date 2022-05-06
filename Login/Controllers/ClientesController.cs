3a46b64f0b51a652b7f641dfad88c03fd82b9a60
WAGNER SOUZA <wagner.souza20@fatec.sp.gov.br>
2022-05-06 16:10:57 -0300

    Add project files.

---
+++ Login/Controllers/ClientesController.cs
@@ -0,0 +1,30 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace Login.Controllers
+{
+    [Authorize(AuthenticationSchemes ="CookieAuthentication")]
+    public class ClientesController : Controller
+    {        public IActionResult Index()
+        {
+            Random rand = new Random();
+            List<ClientesModel> model = new List<ClientesModel>();
+            for (int i = 0; i < rand.Next(20,99); i++)
+            {
+                ClientesModel cliente = new ClientesModel();
+                cliente.CPF = rand.Next(111, 999) + "." + rand.Next(111, 999) + "." + rand.Next(111, 999) + "-" + rand.Next(11, 99);
+                cliente.Nome = "Nome do Cliente " + i;
+                cliente.Telefone = "16" + rand.Next(1000, 9999) + "-" + rand.Next(1000, 9999);
+                cliente.Id = rand.Next(1000, 9999);
+                model.Add(cliente);
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Login/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<Contexto>(a => a.UseSqlServer("Server=c3po;Database=LoginFilmesFavoritos;Trusted_Connection=True;"));
+            services.AddAuthentication("CookieAuthentication")
+                .AddCookie("CookieAuthentication", options =>
+                {
+                    options.AccessDeniedPath = "/Login/Ops/";
+                    options.LoginPath = "/Login/Entrar";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
